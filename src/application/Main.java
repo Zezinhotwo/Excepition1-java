1e7a271707af9325e95d2fb162e83a9bf9e6f96b
Akio <SEU_NOREPLY@users.noreply.github.com>
2025-12-21 02:02:44 -0300

    bad solution

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,33 +17,29 @@ class Main {
         System.out.print("Room number: ");
         int number = sc.nextInt();
         System.out.print("Check-in date: (dd/MM/yyyy): ");
-        Date checkInt = sdf.parse(sc.next());
+        Date checkIn = sdf.parse(sc.next());
         System.out.print("Check-out date: (dd/MM/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
 
-        if (!checkOut.after(checkInt)) {
+        if (!checkOut.after(checkIn)) {
             System.out.println("Error in reservation: Check-out date must be after check-in date");
         } else {
-            Reservation reservation = new Reservation(number, checkInt, checkOut);
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Rerservation: " + reservation);
             System.out.println();
             System.out.println("Enter data  to update the reservation:");
             System.out.print("Check-in date: (dd/MM/yyyy): ");
-            checkInt = sdf.parse(sc.next());
+            checkIn = sdf.parse(sc.next());
             System.out.print("Check-out date: (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkInt.before(now) || checkOut.before(now)) {
-                System.out.println("Error in reservation: Check-out date for update must be after check-in date");
-            } else if (!checkOut.after(checkInt)) {
-                System.out.println("Error in reservation: Check-out date must be future check-in date");
-            } else {
-                reservation.updateDates(checkInt, checkInt);
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("s");
+                System.out.println("Error in reservation: " + error);
+            }else{
                 System.out.println("Rerservation: " + reservation);
             }
-
-
         }
 
         sc.close();
